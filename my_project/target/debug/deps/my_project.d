2d6906538f797bcae6d421c24dd5e0b8ee20ce91
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-02-20 05:37:24 +0500

    Created a new project folder

---
+++ my_project/target/debug/deps/my_project.d
@@ -0,0 +1,5 @@
+D:\Study Shit\Programming Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\target\debug\deps\my_project.exe: src\main.rs
+
+D:\Study Shit\Programming Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\target\debug\deps\my_project.d: src\main.rs
+
+src\main.rs:
---
+++ my_project/target/debug/my_project.d
@@ -0,0 +1,1 @@
+D:\Study\ Shit\Programming\ Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\target\debug\my_project.exe: D:\Study\ Shit\Programming\ Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\src\main.rs
