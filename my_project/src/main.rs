6ddae0c46aa85fcfc410dbc4766ecdebddd86fe7
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-02-20 05:48:50 +0500

    Learned Rust Syntax for declaring variables, creating functions, if else statements, and loops

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Variables
+    let msg = "Hello, world!";
+    let x: i32 = 42;
+    let pi: f64 = 3.14;
+    let is_rust_fun: bool = true;
+    let letter_a: char = 'a';
+
+    // Functions
+    // fn name(parameters) -> return_type {}
+    fn add(x: i32, y: i32) -> i32 {
+        return x + y;
+        // OR
+        x + y // no semicolon so, returns value
+    }
+
+    let x = 42; // variables can be redeclared (overwrites)
+
+    if x >= 0 {
+        println!("X is non-negative");
+    }
+    else {
+        println!("x is negative");
+    }
+
+    let mut i = 1; // mutable variable
+
+    while i<=5 {
+        println!{"{}",i};   // {} is placeholder for i
+        i += 1;
+    }
 }
