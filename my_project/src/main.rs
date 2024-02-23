446c63a176ea699f34a5c189ceac495d3b069ca9
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-02-23 20:50:11 +0500

    Basic Data Types

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -30,4 +30,44 @@ fn main() {
         println!{"{}",i};   // {} is placeholder for i
         i += 1;
     }
+
+    // Data Types
+    
+    //integers can be of 8, 16, 32, 64, or 128 bits (signed or unsigned)
+    let number_of_days: i8 = 7;
+    let number_of_users: i64 = 12000;  // signed (+ or -)
+    let number_of_tokens: u64 = 10000; // unsigned (just +)
+
+    //float can be of 32 or 64 bits
+    let pi: f32 = 3.14;
+
+    // Char
+    let _my_char = 'A';
+
+    //Strings
+    let msg: &str = "Hello";    // immutable
+
+    let mut name = String::from("Alice");   // mutable
+
+    // Arrays
+    let numbers: [i32; 3] = [1,2,3];
+
+    let second_number = numbers[1];
+    println!("The second number in the array is {}.", second_number);
+
+    // Slices
+    let slice = &numbers[1..3];
+    let first_element = slice[0];
+    println!("The first element of the slice is {}.", first_element);
+
+    // Tuples
+    let person = ("Alice", 30);
+
+    let name = person.0;
+    let age = person.1;
+    println!("The person's name is {} and their age is {}.", name, age);
+
+    let person = (("Alice", "Smith"), 30);
+    println!("The person's name is {} {} and their age is {}.", person.0.0, person.0.1, person.1);
+
 }
---
+++ my_project/target/debug/deps/my_project-252e464141592e9c.d
@@ -0,0 +1,5 @@
+D:\Study Shit\Programming Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\target\debug\deps\libmy_project-252e464141592e9c.rmeta: src\main.rs
+
+D:\Study Shit\Programming Shit\Rust-Bootcamp\Rust-Bootcamp-RiseInXGDSC\my_project\target\debug\deps\my_project-252e464141592e9c.d: src\main.rs
+
+src\main.rs:
